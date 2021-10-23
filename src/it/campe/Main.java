a28466087eccf792a85e9f390f75f6ff112b5b77
malyyboh <malyyboh@gmail.com>
2021-10-23 09:44:56 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,7 +3,10 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int intValue = 1;
+        int numericValue = 2;
+        int size = 3;
+        int number = 10;
 
     }
 }
